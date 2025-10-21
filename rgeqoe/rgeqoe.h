112b8e7d50e57110b5e3f0935fb956a1cade68bc
GuillermoPM <guillermopmar@gmail.com>
2025-10-22 00:13:07 +0200

    change comments

--- rgeqoe/rgeqoe.h
+++ rgeqoe/rgeqoe.h
@@ -19,52 +19,52 @@ namespace orb {
 
             std::unique_ptr<Gravity> gravity_model; // Gravity perturbation model
 
-            /*
-            @brief Computes the propagation of the orbit using RGEqOE formulation
-            @param tspan Vector of time points for propagation.
-            @param s0 Initial rgeqoe state.
-            @param dt Time step for integration.
-            @param sprop Output: propagated rgeqoe state.
-            */
+            //
+            // @brief Computes the propagation of the orbit using RGEqOE formulation
+            // @param tspan Vector of time points for propagation.
+            // @param s0 Initial rgeqoe state.
+            // @param dt Time step for integration.
+            // @param sprop Output: propagated rgeqoe state.
+            //
             void compute(std::vector<double> tspan, rgeqoe s0, double dt, std::map<double, rgeqoe> &sprop);
 
-            /*
-            @brief Computes the time derivatives of the RGEqOE state.
-            @param t Current time.
-            @param s Current RGEqOE state.
-            @return Time derivatives of the RGEqOE state.
-            */
+            //
+            // @brief Computes the time derivatives of the RGEqOE state.
+            // @param t Current time.
+            // @param s Current RGEqOE state.
+            // @return Time derivatives of the RGEqOE state.
+            //
             rgeqoe odes(double t, rgeqoe s);
 
-            /*
-            @brief Initializes the RGEqOE state from the given position and velocity vectors.
-            @param rv0 Initial position and velocity vectors.
-            @param t Time at which the state is initialized.
-            @return The initialized RGEqOE state.
-            */
+            //
+            // @brief Initializes the RGEqOE state from the given position and velocity vectors.
+            // @param rv0 Initial position and velocity vectors.
+            // @param t Time at which the state is initialized.
+            // @return The initialized RGEqOE state.
+            //
             posvel init_rgeqoe(posvel rv0, double t);
 
-            /*
-            @brief Updates the reference frame angular velocity with the given time.
-            @param t Current time.
-            */
+            //
+            // @brief Updates the reference frame angular velocity with the given time.
+            // @param t Current time.
+            //
             void update_av(double t);
 
-            /*
-            @brief Converts position and velocity vectors to RGEqOE state.
-            @param R Position vector.
-            @param V Velocity vector.
-            @param t Time at which the state is defined.
-            @return The corresponding RGEqOE state.
-            */
+            //
+            // @brief Converts position and velocity vectors to RGEqOE state.
+            // @param R Position vector.
+            // @param V Velocity vector.
+            // @param t Time at which the state is defined.
+            // @return The corresponding RGEqOE state.
+            //
             rgeqoe state2rgeqoe(Vector3D R, Vector3D V, double t);
 
-            /*
-            @brief Converts RGEqOE state to position and velocity vectors.
-            @param s RGEqOE state.
-            @param t Time at which the state is defined.
-            @return The corresponding position and velocity vectors.
-            */
+            //
+            // @brief Converts RGEqOE state to position and velocity vectors.
+            // @param s RGEqOE state.
+            // @param t Time at which the state is defined.
+            // @return The corresponding position and velocity vectors.
+            //
             posvel rgeqoe2state(rgeqoe s, double t);
 
         private:
